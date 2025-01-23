01f2ae4b67179bb8a7ea222b641a472bf24ece91
Bryan Tai <redhatpieman@gmail.com>
2025-01-23 10:32:54 -0800

    Rearranged folder structure to separate assets by minigame

---
+++ assets/game_manager/scripts/base_mini_game.gd
@@ -0,0 +1,168 @@
+@icon("res://assets/game_manager/BaseMiniGame.svg")
+class_name BaseMiniGame extends Node2D
+
+## A short description of the node will go here.
+# This class handles the base behaviour of all MiniGames
+# All MiniGames should extend this class
+# For examples of how to create a MiniGame, see the Template scripts
+ 
+## A longer description of the BaseMiniGame node will go here.
+
+## A signal that should be emitted when the mini-game is finished, along with a 
+## [bool] parameter specifying if the player has won ([code]TRUE[/code]) or lost 
+## ([code]FALSE[/code]).
+signal game_finished(is_win: bool)
+
+## These are the numerous states we'll use.
+enum MiniGameState { 
+	PREPARING, ## The state that the minigame first loads in, shows the instructions.
+	PLAYING, ## The state that contains the actual minigame.
+	WIN, ## The state when a player achieves the [color=lime]WIN[/color] condition.
+	LOSE ## The state when the player has [color=red]LOST[/color] or run out of time.
+}
+
+## The text that will be displayed during the [code]PREPARING[/code] state.
+@export var instruction_text: String = "WIN" # Replace this text in the Inspector!
+
+## The current state of the minigame. This will always start with [code]PREPARING[/code].
+var current_state: MiniGameState = MiniGameState.PREPARING
+
+## Set this flag to "false" in your MiniGame if you don't want everything disabled outside of the PLAYING state
+var disable_minigame_during_intro_and_outro = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_on_start_preparing_state()
+
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	process_state_machine(delta)
+
+## This will begin the actual Minigame specified in the [enum MiniGameState]'s  
+## [code]PLAYING[/code] state.
+func start_playing_game() -> void:
+	set_mini_game_state(MiniGameState.PLAYING)
+
+# ENDING THE GAME
+
+## A signal from the MiniGameManager that time has run out.
+## Can be overridden if the MiniGame has different criteria, where surviving for
+## the duration of the timer is the WIN condition, for example.
+func _on_timeout() -> void:
+	trigger_game_lose()
+
+## Notifies the Game Manager that the WIN condition has been met.
+func trigger_game_win() -> void:
+	game_finished.emit(true)
+	set_mini_game_state(MiniGameState.WIN)
+
+## Notifies the Game Manager that the WIN condition was [b]NOT[/b] met.
+func trigger_game_lose() -> void:
+	game_finished.emit(false)
+	set_mini_game_state(MiniGameState.LOSE)
+
+# STATE MACHINE FUNCTIONS
+
+## Switches the current state
+func set_mini_game_state(new_state: MiniGameState) -> void:
+	if(current_state == new_state):
+		return
+
+	var old_state = current_state
+	current_state = new_state
+	
+	match old_state:
+		MiniGameState.PREPARING:
+			_on_end_preparing_state()
+		MiniGameState.PLAYING:
+			_on_end_playing_state()
+	
+	match current_state:
+		MiniGameState.PLAYING:
+			_on_start_playing_state()
+		MiniGameState.WIN:
+			_on_start_win_state()
+		MiniGameState.LOSE:
+			_on_start_lose_state()
+
+## Update the minigame based on the current state
+func process_state_machine(delta: float) -> void:
+	match(current_state):
+		MiniGameState.PREPARING:
+			_process_preparing_state(delta)
+		MiniGameState.PLAYING:
+			_process_playing_state(delta)
+		MiniGameState.WIN:
+			_process_win_state(delta)
+		MiniGameState.LOSE:
+			_process_lose_state(delta)
+
+# STATE SPECIFIC FUNCTIONS
+#
+# NOTE: All the functions below this line will handle the different state events
+# They are intended to be overridden by new MiniGame scripts!
+
+# PREPARE STATE
+
+## Called once when entering the PREPARING state (e.g. once the minigame is loaded)
+## almost equivalent to _ready()
+func _on_start_preparing_state() -> void:
+	# This disables every Node in the mini game
+	if(disable_minigame_during_intro_and_outro):
+		process_mode = Node.PROCESS_MODE_DISABLED
+
+## Called every frame while minigame is in the PREPARING state. This method should
+## contain any special code beyond the default instructions BEFORE the game starts.
+## [br]
+## e.g. Kirby crashing into the stage on his shooting star.
+func _process_preparing_state(delta: float) -> void:
+	pass
+
+## Called once when the PREPARING state ends. Could be used to trigger some animation
+## to signal that the mini-game has started.
+## [br]
+## e.g. a gun firing off and then disappearing.
+func _on_end_preparing_state() -> void:
+	# Once the instruction banner goes away, enable all the nodes in the game
+	if(disable_minigame_during_intro_and_outro):
+		process_mode = Node.PROCESS_MODE_INHERIT
+
+# PLAYING STATE
+
+## Called once when entering the PLAYING state (e.g. once the player gains control)
+func _on_start_playing_state() -> void:
+	pass
+
+## Called every frame while minigame is in the PLAYING state. This is where the 
+## meat of the mini-game should be!
+func _process_playing_state(delta: float) -> void:
+	pass
+
+## Called once when the PLAYING state ends. You could trigger any number of things
+## here before the actual WIN/LOSE window appears.
+## [br]
+## e.g. your running character screeches to a halt over a short distance.
+func _on_end_playing_state() -> void:
+	# Disables every Node in the mini game on Win or Lose
+	if(disable_minigame_during_intro_and_outro):
+		process_mode = Node.PROCESS_MODE_DISABLED
+
+# WIN STATE
+
+## Called once when entering the WIN state.
+func _on_start_win_state() -> void:
+	pass
+
+## Called every frame while minigame is in the WIN state.
+func _process_win_state(delta: float) -> void:
+	pass
+
+# LOSE STATE
+
+## Called once when entering the LOSE state.
+func _on_start_lose_state() -> void:
+	pass
+
+## Called every frame while minigame is in the LOSE state.
+func _process_lose_state(delta: float) -> void:
+	pass
---
+++ assets/game_manager/scripts/mini_game_manager.gd
@@ -0,0 +1,150 @@
+class_name MiniGameManager
+extends Node2D
+
+const TIME_LIMIT: float = 5.0
+const PREPARE_TIME: float = 1.0
+const MINI_GAME_FOLDER_PATH: String = "res://minigame_scenes/"
+
+const ANIM_INTRO = "intro_manager"
+const ANIM_TRANSITION = "transition_mini_game"
+const ANIM_INSTRUCTION = "show_instruction_banner"
+const ANIM_END = "show_end_banner"
+
+enum ManagerStates {
+	INTRO,
+	TRANSITIONING,
+	PREPARING,
+	PLAYING,
+	ENDING,
+}
+
+var current_manager_state: ManagerStates = ManagerStates.INTRO 
+
+@onready var mini_game_timer: Timer = $MiniGameTimer
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+## Overlay UI elements
+@onready var overlay_mini_game: CanvasLayer = $MiniGameOverlay
+@onready var timer_label: Label = $MiniGameOverlay/TimerLabel
+@onready var instruction_label: Label = $MiniGameOverlay/InstructionBanner/InstructionLabel
+@onready var instruction_banner: Control = $MiniGameOverlay/InstructionBanner
+
+var rng = RandomNumberGenerator.new()
+
+var current_mini_game: BaseMiniGame
+var current_mini_game_resource: Resource # Next minigames are loaded during the TRANSITION state
+
+var all_mini_game_names: PackedStringArray
+var all_mini_game_count: int
+
+## Fired if the mini_game_timer runs out before the current_mini_game responds
+signal mini_game_timeout
+
+func cache_all_mini_game_names() -> void:
+	var dir = DirAccess.open(MINI_GAME_FOLDER_PATH)
+	if dir:
+		all_mini_game_names = dir.get_files()
+		all_mini_game_count = all_mini_game_names.size()
+		#for str in all_mini_game_names:
+		#	print(str)
+	else:
+		print("ERROR: Cannot find MiniGame folder!")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	## Manager starts in the INTRO state
+	mini_game_timer.wait_time = TIME_LIMIT
+	overlay_mini_game.visible = false
+	instruction_banner.visible = false
+	cache_all_mini_game_names()
+	animation_player.play(ANIM_INTRO) # Start the Intro
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if(current_manager_state == ManagerStates.PLAYING):
+		update_timer()
+	
+func update_timer() -> void:
+	set_timer_text(str((mini_game_timer.time_left + 1) as int))
+
+func set_timer_text(time_text: String) -> void:
+	timer_label.text = "TIME: " + time_text 
+
+func pick_next_mini_game_name() -> String:
+	#TODO Once we have more minigames, create a buffer Queue to reduce chances of repeats
+	var next_index = rng.randi_range(0, all_mini_game_count - 1)
+	return all_mini_game_names[next_index]
+	#return "meteor_mini_game.tscn"
+
+## Loads up the next randomly picked minigame in the background
+func load_next_mini_game() -> void:
+	if(current_mini_game != null):
+		current_mini_game.queue_free() # Delete the previous minigame
+	
+	var filepath = MINI_GAME_FOLDER_PATH + pick_next_mini_game_name()
+	current_mini_game_resource = load(filepath)
+	
+## Instantiates the loaded minigame, and adds it to the manager
+func create_next_mini_game() -> void:
+	current_mini_game = current_mini_game_resource.instantiate() as BaseMiniGame
+	add_child(current_mini_game)
+	current_mini_game.game_finished.connect(_on_mini_game_finished)
+	instruction_label.text = current_mini_game.instruction_text
+
+## Gives control to the Player
+func start_current_mini_game() -> void:
+	instruction_banner.visible = false
+	current_mini_game.start_playing_game()
+	mini_game_timer.wait_time = TIME_LIMIT
+	mini_game_timer.start()
+
+## Manages
+func set_manager_state(new_state: ManagerStates) -> void:
+	if(current_manager_state == new_state):
+		return
+	
+	var old_state = current_manager_state
+	current_manager_state = new_state
+	
+	## On Start State
+	match current_manager_state:
+		ManagerStates.TRANSITIONING:
+			timer_label.visible = false
+			load_next_mini_game()
+			animation_player.play(ANIM_TRANSITION)
+		ManagerStates.PREPARING:
+			set_timer_text(str(TIME_LIMIT as int))
+			timer_label.visible = true
+			create_next_mini_game()
+			overlay_mini_game.visible = true
+			animation_player.play(ANIM_INSTRUCTION)
+		ManagerStates.PLAYING:
+			start_current_mini_game()
+		ManagerStates.ENDING:
+			animation_player.play(ANIM_END)
+
+func _on_animation_player_finished(anim_name: StringName) -> void:
+	## On End State
+	match anim_name:
+		ANIM_TRANSITION:
+			set_manager_state(ManagerStates.PREPARING)
+		ANIM_INTRO,	ANIM_END:
+			set_manager_state(ManagerStates.TRANSITIONING)
+		ANIM_INSTRUCTION:
+			set_manager_state(ManagerStates.PLAYING)
+
+## The current minigame will either end when time runs out ...
+func _on_mini_game_timer_timeout() -> void:
+	set_timer_text("OUT")
+	current_mini_game._on_timeout()
+
+## ... or when the current_mini_game signals the Manager that the player reached a Win/Lose state
+func _on_mini_game_finished(is_win: bool) -> void:
+	mini_game_timer.stop()
+	# TODO: Track wins and loses here!
+	if (is_win == true):
+		instruction_label.text = "WINNER!"
+	else:
+		instruction_label.text = "LOSER!"
+	set_manager_state(ManagerStates.ENDING)
