18e284f1e27134107c462bfde191343cc3c1162b
Tearany <doggybird2463@yahoo.ca>
2025-01-24 23:09:56 -0400

    new graphics and sound, added cursor, fixed for The Square 🟦
    cursor's still broke but its mostly done

--- assets/gozi_bubblewrap_minigame/scripts/gozi_bubblewrap_bubble.gd
+++ assets/gozi_bubblewrap_minigame/scripts/gozi_bubblewrap_bubble.gd
@@ -1,12 +1,25 @@
 extends Node2D
 
 var popped = false
+var is_hovering = false
 
-#func _process_playing_state(delta: float) -> void:
-	#if $AnimatedSprite2D.animation == "whole1" and popped == true:
-		
+func _on_start_playing_state():
+	if popped == false:
+		$AnimatedSprite2D/Area2D/CollisionShape2D.set_process(false)
+		print("it's off")
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and $AnimatedSprite2D.animation == "whole1":
+	if Input.is_action_just_pressed("fire") and $AnimatedSprite2D.animation == "whole1" and is_hovering:
 		popped = true
 		$AnimatedSprite2D.play("popped2")
+		$AudioStreamPlayer.play()
+		
+func _on_area_entered(area):
+	is_hovering = true
+
+func _on_area_exited(area):
+	is_hovering = false
+
+
+func _on_area_2d_area_entered(area):
+	pass # Replace with function body.
