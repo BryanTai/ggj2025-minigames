ae0db20e2a46d23ccd29f4a44569aabffdce665e
Bryan Tai <redhatpieman@gmail.com>
2025-01-25 01:49:52 -0800

    updated ballcup sprites, added cup swapping animations

--- assets/ball_cup_minigame/ball_cup_mini_game.gd
+++ assets/ball_cup_minigame/ball_cup_mini_game.gd
@@ -3,8 +3,11 @@ extends BaseMiniGame
 @onready var cups_parent: Node = $Cups
 @onready var ball: Sprite2D = $Ball
 @onready var hand: Sprite2D = $Hand
+@onready var swapping_timer: Timer = $SwappingTimer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 const HAND_Y_POS = 725
+const BALL_Y_OFFSET = 70
 const MOUSE_X_BUFFER = 50
 
 var cup_positions: Array[Vector2]
@@ -15,6 +18,8 @@ var ball_index: int
 var hand_index: int
 var ball_win_index: int
 
+var swaps_made: int
+@export var total_swaps: int = 5
 
 var can_guess: bool = false
 
@@ -27,12 +32,48 @@ var can_guess: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	instruction_text = "Find the Ball!"
-	super()
 	init_positions()
+	cups[1].visible = false
+	super()
+	set_ball_position(1)
+
+func _on_start_playing_state():
+	cups[1].visible = true
+	cups[1].lower_cup()
 	
-func _on_swapping_complete():
+func make_random_swap():
+	swaps_made += 1
+	var first_index = randi_range(0,2)
+	var second_index = randi_range(0,2)
+	if(first_index == second_index):
+		swap_all_cups()
+	else:
+		swap_cups(min(first_index, second_index), max(first_index, second_index))
+
+func swap_cups(first:int, second:int):
+	var animation_name = "swap_cups_" + str(first) + "_" + str(second)
+	animation_player.play(animation_name)
+	if(ball_index == first):
+		ball_index = second
+	elif(ball_index == second):
+		ball_index = first
+
+func swap_all_cups():
+	animation_player.play("swap_all_cups")
+	ball_index += 1
+	if(ball_index > 2):
+		ball_index = 0
+		
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	#animation_player.play("RESET")
+	if(swaps_made < total_swaps):
+		make_random_swap()
+	else:
+		_on_all_swaps_complete()
+	
+func _on_all_swaps_complete():
+	set_ball_position(ball_win_index)
 	can_guess = true
-	
 
 func init_positions():
 	var all_cup_nodes = cups_parent.get_children()
@@ -43,7 +84,8 @@ func init_positions():
 		hand_positions.append(Vector2(cup_node_2d.position.x, HAND_Y_POS))
 
 func set_ball_position(index:int):
-	ball.position = cup_positions[index]
+	var cup_pos = cup_positions[index]
+	ball.position = Vector2(cup_pos.x, cup_pos.y + BALL_Y_OFFSET)
 	ball_index = index
 	ball_win_index = index
 	
