f8626eb55c064959fb59f2dbab2c2f8bcf4f79b5
Michael Wright <beamer1989@gmail.com>
2025-01-25 16:45:46 -0800

    lark game fixes

--- assets/larkBubblePopper/scripts/lark_bubble_popper.gd
+++ assets/larkBubblePopper/scripts/lark_bubble_popper.gd
@@ -56,6 +56,8 @@ extends BaseMiniGame
 var larkBubble = load("res://assets/larkBubblePopper/scenes/lark_bubble.tscn")
 var bubblesPopped = 0
 var currentFrame = 0
+var catBubbleCount = 0
+var hasWon = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -70,8 +72,8 @@ func _process(delta: float) -> void:
 	currentFrame += 1
 	# Bouncy: Tell me. For whom do you fight? Hmph! How very glib. And do you believe in Eorzea? Eorzea's unity is forged of falsehoods. Its city-states are built on deceit. And its faith is an instrument of deception. It is naught but a cobweb of lies. To believe in Eorzea is to believe in nothing. In Eorzea, the beast tribes often summon gods to fight in their stead--though your comrades only rarely respond in kind. Which is strange, is it not? Are the "Twelve" otherwise engaged? I was given to understand
 	# Gogozi: they were your protectors. If you truly believe them your guardians, why do you not repeat the trick that served you so well at Carteneau, and call them down? They will answer--so long as you lavish them with crystals and gorge them on aether. Your gods are no different than those of the beasts--eikons every one. Accept but this, and you will see how Eorzea's faith is bleeding the land dry. Nor is this unknown to your masters. Which prompts the question: Why do they cling to these false deities
-	if(currentFrame % 50) == 0:
-		for i in 4:
+	if(currentFrame % 200) == 0:
+		for i in 5:
 			# demonknighz: creemdom
 			var bubbleinstance = larkBubble.instantiate() as Area2D
 			$".".add_child(bubbleinstance)
@@ -79,7 +81,12 @@ func _process(delta: float) -> void:
 			bubbleinstance.set_position(randomPos)
 			print(currentFrame)
 	
-	
+func _on_end_playing_state() -> void:
+	if(catBubbleCount == 0):
+		hasWon = true
+		trigger_game_win()
+	else:
+		trigger_game_lose()
 	
 	# Here's an example
 	## TODO: Use these 2 functions to trigger WIN or LOSE
@@ -89,11 +96,8 @@ func _process(delta: float) -> void:
 	#   trigger_game_win()
 
 func bubbleWasPopped() -> void:
-	bubblesPopped += 1
-	print(bubblesPopped)
-	if(bubblesPopped > 4):
-		# vongoyyt - https://archive.org/stream/bee-movie-2007_202405/Bee%20Movie%20Script_djvu.txt
-		trigger_game_win()
+	$popnoise.play()
+	catBubbleCount -= 1
 	
 
 # A signal from the MiniGameManager that time has run out
