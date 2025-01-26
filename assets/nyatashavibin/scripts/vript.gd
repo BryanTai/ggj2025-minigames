9b7d1da2a5943088938ce876464b6c6e1b866a91
Michael Wright <beamer1989@gmail.com>
2025-01-26 03:22:30 -0800

    nyatasha VIBIN
    she be doin

---
+++ assets/nyatashavibin/scripts/vript.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var goodvibes = ["slimes", "bubs", "anime"]
+var badvibes = ["twitter", "taxes", "low batteries"]
+var isbad = false
+@onready var vabel: Label = $vabel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var badnumber = randi_range(0,1)
+	if(badnumber == 0):
+		isbad = false
+	else:
+		$vubble.texture = load("res://assets/nyatashavibin/sprites/BadBub.png")
+		isbad = true
+	if(isbad):
+		vabel.text = badvibes[randi_range(0, badvibes.size() -1)]
+	else:
+		vabel.text = goodvibes[randi_range(0, badvibes.size() -1)]
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	global_position = global_position.move_toward(Vector2(511,537), delta * 200)
+	
