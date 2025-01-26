75f9d9fa5814e8d507de0a0afd84928675063c6b
Petrichordate <thatwasyourlife@gmail.com>
2025-01-25 21:29:36 -0800

    Menu

--- assets/game_manager/menucursor.gd
+++ assets/game_manager/menucursor.gd
@@ -1,19 +1,41 @@
 extends RigidBody2D
 
-var speed =200
-var velocity=Vector2()
-var mouse_position = null
+var SPEED = 600
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+func _process(delta):
+	
+	var directionx := Input.get_axis("move_left", "move_right")
+	var directiony := Input.get_axis("move_up","move_down")
+	
+	#	if inpu_event(""):
+	if Input.is_action_pressed("fire"):
+		animated_sprite_2d.animation = "click"
+	else:
+		animated_sprite_2d.animation = "default"
+	
 
-# Called when the node enters the scene tree for the first time.
-func _physics_process(delta) -> void:
-	#velocity = Vector2(0,0)
-	mouse_position=get_global_mouse_position()
+	if directionx:
+		linear_velocity.x=directionx *SPEED
+	else:
+		linear_velocity.x = move_toward(linear_velocity.x, 0, SPEED)
+				
+	if directiony:
+		linear_velocity.y=directiony *SPEED
+
+	else:
+		linear_velocity.y = move_toward(linear_velocity.y, 0, SPEED)
+
 	
-	var direction = (mouse_position-position).normalized()
-	velocity=(direction*speed*delta)
-		
 	
-	
-	#move_and_slide(velocity)
-			
+#var mouseposition
+#var cursordirection=Vector2(0,0)
+  	
+#func _process(delta):
+#		mouseposition = get_local_mouse_position()
+#		
+#		cursordirection=mouseposition-position
+#				
+#		#rotation+= mouseposition.angle() * 0.1
+#		position+= cursordirection
+#		print (cursordirection)
