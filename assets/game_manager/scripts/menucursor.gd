51e1031cc7b1e8bdb9be6f71662aceb7bd38bd41
Nic D. <91558719+Venti-Bear@users.noreply.github.com>
2025-01-26 03:00:54 -0500

    Added mouse controls to Menu
    Should work,

--- assets/game_manager/scripts/menucursor.gd
+++ assets/game_manager/scripts/menucursor.gd
@@ -1,41 +1,35 @@
 extends RigidBody2D
 
 var SPEED = 600
+var mouse_pos: Vector2
+var is_using_mouse: bool = false
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _process(delta):
 	
-	var directionx := Input.get_axis("move_left", "move_right")
-	var directiony := Input.get_axis("move_up","move_down")
+	var direction := Vector2(Input.get_axis("move_left", "move_right"), \
+							 Input.get_axis("move_up","move_down"))
 	
-	#	if inpu_event(""):
+	if direction and is_using_mouse:
+		is_using_mouse = false
+	
 	if Input.is_action_pressed("fire"):
 		animated_sprite_2d.animation = "click"
 	else:
 		animated_sprite_2d.animation = "default"
 	
 
-	if directionx:
-		linear_velocity.x=directionx *SPEED
+	if direction:
+		linear_velocity=direction *SPEED
+	elif is_using_mouse:
+		global_position = global_position.move_toward(mouse_pos, delta * SPEED)
 	else:
 		linear_velocity.x = move_toward(linear_velocity.x, 0, SPEED)
-				
-	if directiony:
-		linear_velocity.y=directiony *SPEED
-
-	else:
 		linear_velocity.y = move_toward(linear_velocity.y, 0, SPEED)
-
-	
-	
-#var mouseposition
-#var cursordirection=Vector2(0,0)
   	
-#func _process(delta):
-#		mouseposition = get_local_mouse_position()
-#		
-#		cursordirection=mouseposition-position
-#				
-#		#rotation+= mouseposition.angle() * 0.1
-#		position+= cursordirection
-#		print (cursordirection)
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		mouse_pos = event.position
+		is_using_mouse = true
