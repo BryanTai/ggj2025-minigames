c65ccd981969e051f42d6db0c0625cc259f93725
Bryan Tai <BryanTai@users.noreply.github.com>
2025-01-25 21:39:02 -0800

    Fixed missing BubbleWare collab sound

--- assets/game_manager/scripts/audio_stream_player_2d.gd
+++
@@ -1,9 +0,0 @@
-extends AudioStreamPlayer2D
-
-@onready var audio_stream_player_2d: AudioStreamPlayer2D = $"."
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	audio_stream_player_2d.play()
-	
-	
