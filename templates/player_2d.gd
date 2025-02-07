cceea4cd629e6ca6c74c273f0724e6081e0ab75d
Nic D. <91558719+Venti-Bear@users.noreply.github.com>
2025-02-07 15:06:17 -0500

    Bug fix milk bubbles, pet dog
    - Adjusted movement speed for hand in dog game, also simplified how movement is handled in Player2D template
    - Changed Particle emitter in milk bubbles game to CPUParticles2D

--- templates/player_2d.gd
+++ templates/player_2d.gd
@@ -16,26 +16,14 @@ func _process(delta: float) -> void:
 	if(not movement_enabled):
 		return
 	
-	# This handles Horizontal movement
-	var horizontal_direction := Input.get_axis("move_left", "move_right")
-	if(horizontal_direction != 0):
-		var old_pos = position
-		var new_x = old_pos.x + (horizontal_direction * player_speed * delta);
-		set_position(Vector2(new_x, old_pos.y))
-		mouse_priority = false
-	
-	# This handles Vertical movement
-	var vertical_direction := Input.get_axis("move_up", "move_down")
-	if(vertical_direction != 0):
-		var old_pos = position
-		var new_y = old_pos.y + (vertical_direction * player_speed * delta);
-		set_position(Vector2(old_pos.x, new_y))
-		mouse_priority = false
-	
-	# This will move the character player towards the mouse cursor
-	# ATTENTION: Replace "player_speed" with whatever speed you have in your minigame!
-	elif target_pos and mouse_priority:
+	if mouse_priority:
 		global_position = global_position.move_toward(target_pos, player_speed * delta)
+	else:
+		var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		
+		if direction:
+			position += direction * player_speed * delta
+	
 
 ## Defines a target_pos variable to match the mouse cursor's position if mouse
 ## movement is detected.
