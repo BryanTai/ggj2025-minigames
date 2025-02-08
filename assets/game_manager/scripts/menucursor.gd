1484063a86ffd0d0d48f0ee2bcf53b65ce0b8944
Nic D. <91558719+Venti-Bear@users.noreply.github.com>
2025-02-08 09:07:44 -0500

    Add controller/keyboard functionality to main menu
    Took a bit of trial and error, but pressing the "fire" action in the main menu now simulates a left click being pressed at the paw cursor's location, regardless of input device.

--- assets/game_manager/scripts/menucursor.gd
+++ assets/game_manager/scripts/menucursor.gd
@@ -11,13 +11,18 @@ func _process(delta):
 	
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	if Input.is_action_pressed("fire"):
+	if Input.is_action_just_pressed("fire"):
 		animated_sprite_2d.animation = "click"
-	else:
+		_mouse_click()
+		print("pressed at: ", position)
+	
+	if Input.is_action_just_released("fire"):
 		animated_sprite_2d.animation = "default"
 
 	if direction:
 		linear_velocity = direction *SPEED
+		get_viewport().warp_mouse(global_position)
+		is_using_mouse = false
 	elif is_using_mouse:
 		global_position = global_position.move_toward(mouse_pos, delta * SPEED)
 	else:
@@ -25,8 +30,27 @@ func _process(delta):
 		linear_velocity.y = move_toward(linear_velocity.y, 0, SPEED)
   	
 func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		_mouse_release()
+	
 	if event is InputEventMouseMotion:
 		mouse_pos = event.position
 		is_using_mouse = true
-	else:
-		is_using_mouse = false
+
+func _mouse_click() -> void:
+	var click := InputEventMouseButton.new()
+	click.button_index = 1
+	click.button_mask = MOUSE_BUTTON_MASK_LEFT
+	click.pressed = true
+	click.position = position
+	Input.parse_input_event(click)
+	print("click")
+	
+func _mouse_release() -> void:
+	var click := InputEventMouseButton.new()
+	click.button_index = 1
+	click.button_mask = 0 
+	click.pressed = false
+	click.position = position
+	Input.parse_input_event(click)
+	print("unclick")
