dd26e0b2b3ec25f69c5c077baa1d22e0fba6a0a6
Nic D. <91558719+Venti-Bear@users.noreply.github.com>
2025-02-08 16:43:34 -0500

    Minor adjustments to menu

--- assets/game_manager/scripts/option select.gd
+++ assets/game_manager/scripts/option select.gd
@@ -1,18 +1,12 @@
 extends Area2D
 
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var animated_sprite_2d = $AnimatedSprite2D
-var is_hovering = false
-
 signal play_button_pressed
 
-var already_pressed = false
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
-func _on_area_entered(_area: Area2D) -> void:
-	is_hovering = true
+var is_hovering = false
 
-func _on_area_exited(_area: Area2D) -> void:
-	is_hovering = false
 
 func _on_body_entered(_body: Node2D) -> void:
 	is_hovering = true
@@ -21,13 +15,10 @@ func _on_body_exited(_body: Node2D) -> void:
 	is_hovering = false
 
 func _process(_delta:float):
-#Animate the Button
-	if (not already_pressed and is_hovering==true and Input.is_action_just_pressed("fire")):
+	if is_hovering and Input.is_action_just_released("fire"):
 		animated_sprite_2d.animation=("click")
 		play_button_pressed.emit()
-		print ("click! bubbleware")
-		already_pressed = true
-	elif is_hovering == true:
+	elif is_hovering:
 		animated_sprite_2d.animation=("hover")
 	else:
 		animated_sprite_2d.animation=("default")
